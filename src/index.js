600c9c897f44451f05a42f40bb75e0327b7ec6f6
Ivan Senchukov <Iasench@yandex.ru>
2020-06-13 12:44:39 +0300

    module1-task3  - исправление замечаний код-ревью

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/app.jsx";
 
-const Settings = {
-  FOUND_PLACES_COUNT: 286
+const settings = {
+  foundPlacesCount: 286
 };
 
 ReactDOM.render(
-    <App foundPlacesCount={Settings.FOUND_PLACES_COUNT}/>,
+    <App foundPlacesCount={settings.foundPlacesCount}/>,
     document.querySelector(`#root`)
 );
