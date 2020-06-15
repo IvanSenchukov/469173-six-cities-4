ff340c74d3e530abf5560679bd6461e732de6a5b
Ivan Senchukov <Iasench@yandex.ru>
2020-06-15 09:44:14 +0300

    module2-task1 - Валидируй, как никогда не валидировал  - Создан компонент place-card.jsx  - Указаны параметры пропсов, применяемых для компонентов place-card.jsx, app.jsx, main.jsx;  - Данные по доступным предложениям передаются в приложение из index.js

---
+++ src/components/app/app.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Main from "../main/main.jsx";
+
+const App = (props) => {
+  return (
+    <Main foundPlacesCount={props.foundPlacesCount} availableOffers={props.availableOffers}/>
+  );
+};
+
+App.PropTypes = {
+  foundPlacesCount: PropTypes.number.isRequired,
+  availableOffers: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        reference: PropTypes.string.isRequired,
+        imageSrc: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        period: PropTypes.string.isRequired,
+        addedToBookmarks: PropTypes.boolean,
+        premium: PropTypes.boolean,
+        rating: PropTypes.number.isRequired,
+      })
+  )
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/app.jsx";
+import App from "./components/app/app.jsx";
 
 const settings = {
-  foundPlacesCount: 286
+  foundPlacesCount: 286,
+  availableOffers: [
+    {
+      id: `0`,
+      name: `Wood and stone place`,
+      type: `Private room`,
+      reference: `#`,
+      imageSrc: `img/room.jpg`,
+      price: 80,
+      period: `night`,
+      addedToBookmarks: true,
+      premium: false,
+      rating: 4,
+    },
+    {
+      id: `1`,
+      name: `Canal View Prinsengracht`,
+      type: `Apartment`,
+      reference: `#`,
+      imageSrc: `img/apartment-02.jpg`,
+      price: 132,
+      period: `night`,
+      addedToBookmarks: false,
+      premium: false,
+      rating: 4,
+    },
+    {
+      id: `3`,
+      name: `Nice, cozy, warm big bed apartment`,
+      type: `Apartment`,
+      reference: `#`,
+      imageSrc: `img/apartment-03.jpg`,
+      price: 180,
+      period: `night`,
+      addedToBookmarks: false,
+      premium: true,
+      rating: 5,
+    },
+  ]
 };
 
 ReactDOM.render(
-    <App foundPlacesCount={settings.foundPlacesCount}/>,
+    <App foundPlacesCount={settings.foundPlacesCount} availableOffers={settings.availableOffers}/>,
     document.querySelector(`#root`)
 );
