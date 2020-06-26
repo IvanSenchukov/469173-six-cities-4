3b8ba535965d0227f416358e98496e62a5950f3a
Ivan Senchukov <Iasench@yandex.ru>
2020-06-26 13:16:57 +0300

    module2-task2 - Под покровом тестов  - Исправления замечаний код-ревью:    - Исправлено именование обработчиков событий;    - Применены шаблоны для формирования тестовых данных;    - В e2e тесте применены методы simulate() и expect().toHaveBeenCalledTimes;

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -27,7 +27,7 @@ const Main = (props) => {
           <div className="container">
             <div className="header__wrapper">
               <div className="header__left">
-                <a className="header__logo-link header__logo-link--active" onClick={props.headerLogoClickHandler}>
+                <a className="header__logo-link header__logo-link--active" onClick={props.onHeaderLogoClick}>
                   <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"></img>
                 </a>
               </div>
@@ -136,7 +136,7 @@ Main.propTypes = {
         rating: PropTypes.number.isRequired,
       })
   ),
-  headerLogoClickHandler: PropTypes.func.isRequired
+  onHeaderLogoClick: PropTypes.func.isRequired
 };
 
 export default Main;
