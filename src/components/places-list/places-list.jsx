a5956a286e7a5417467a2d484e1b92a9113e13a3
Ivan Senchukov <Iasench@yandex.ru>
2020-07-05 11:16:56 +0300

    module3-task1 - создал компонент places-list.jsx. Обработал onMouseOver на предложении.

---
+++ src/components/places-list/places-list.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PlaceCard from "../place-card/place-card.jsx";
+import PropTypes from "prop-types";
+
+class PlacesList extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeCard: {}
+    };
+  }
+
+  render() {
+
+    const handlePlaceCardMouseOver = (offer) => {
+      this.setState({activeCard: offer ? offer : {}});
+    };
+
+    return (
+      <div className="cities__places-list places__list tabs__content">
+        {this.props.offers && this.props.offers.map((offer) => (
+          <PlaceCard
+            key={offer.id}
+            offer={offer}
+            onNameClick={this.props.onPlaceCardNameClick}
+            onMouseOver={handlePlaceCardMouseOver}
+          />)
+        )}
+      </div>
+    );
+  }
+}
+
+PlacesList.propTypes = {
+  offers: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        reference: PropTypes.string.isRequired,
+        imageSrc: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        period: PropTypes.string.isRequired,
+        addedToBookmarks: PropTypes.boolean,
+        premium: PropTypes.boolean,
+        rating: PropTypes.number.isRequired,
+      })
+  ),
+  onPlaceCardNameClick: PropTypes.func.isRequired
+};
+
+export default PlacesList;
