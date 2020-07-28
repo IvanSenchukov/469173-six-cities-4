e6c022cc892d8da884b86295c40a424e7112e783
Ivan Senchukov <Iasench@yandex.ru>
2020-07-28 21:01:01 +0300

    module5-task1 - создан reducer.js

---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  city: {
+    name: `Amsterdam`,
+    coordinates: [52.38333, 4.9],
+  },
+};
+
+const actionType = {};
+
+const actionCreators = {};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export {reducer, actionType, actionCreators};
