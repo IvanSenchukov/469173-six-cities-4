853a1c847d27725251755468c360aacbcae950b0
Ivan Senchukov <Iasench@yandex.ru>
2020-07-31 19:03:49 +0300

    module5-task1 - подключение Redux:  - Создан стэйт;  - Подключены девтулзы;  - Компонент App обёрнут в Provider.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import offers from "./mocks/offers";
+import {createStore} from "redux";
+import {reducer} from "./reducer";
+import {Provider} from "react-redux";
 
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
 
 ReactDOM.render(
-    <App foundPlacesCount={286} offers={offers} onPlaceCardNameClick={() => {}}/>,
+    <Provider store={store}>
+      <App foundPlacesCount={286} offers={offers} onPlaceCardNameClick={() => {}}/>
+    </Provider>,
     document.querySelector(`#root`)
 );
